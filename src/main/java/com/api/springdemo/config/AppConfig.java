3d9c04b4a257a53733bd857174b15f2e1672dde6
hulkhogan82 <hulkhogan82@yahoo.com>
2019-11-24 11:56:24 -0500

    Data Initilizer & AppConfig

---
+++ src/main/java/com/api/springdemo/config/AppConfig.java
@@ -0,0 +1,38 @@
+package com.api.springdemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConfig {
+    
+    @Autowired
+    private Environment env;
+
+    public String GetAppName()
+    {
+        return env.getProperty("app.name");
+    }
+
+    public String GetAppDesc()
+    {
+        return env.getProperty("app.description");
+    }
+
+    @Value("${app.name}")
+    private String appNameByValue;
+
+    @Value("${app.description}")
+    private String appDescByValue;
+    
+    public String getAppNameByValue()
+    {
+        return appNameByValue;
+    }
+    public String getAppDescByValue()
+    {
+        return appDescByValue;
+    }
+}
