b9f5aab623dbbd5197aac1beca40a288330915c9
hulkhogan82 <hulkhogan82@yahoo.com>
2019-11-24 08:39:24 -0500

    pom.xml dependencies updated

---
+++ src/main/java/com/api/springdemo/presentation/QuoteController.java
@@ -0,0 +1,7 @@
+package com.api.springdemo.presentation;
+
+
+public class QuoteController
+{
+
+}
--- src/test/java/com/api/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/api/springdemo/SpringdemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.api.springdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class SpringdemoApplicationTests {
 
 	@Test
@@ -11,3 +11,4 @@ class SpringdemoApplicationTests {
 	}
 
 }
+*/
